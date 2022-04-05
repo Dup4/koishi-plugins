d889671a74a35957704df9cff0ce5ad85af15e99
Dup4 <lyuzhi.pan@gmail.com>
2022-04-06 00:11:34 +0800

    docs: add chromium install ref

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# https://www.itzgeek.com/how-tos/linux/centos-how-tos/how-to-run-google-chrome-as-root-fedora-16-centos-6-rhel-6.html
+# https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md
+
 apt update
 
 apt install -y \
